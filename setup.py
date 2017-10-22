fd6e937fa96f3d6b4d010a424d561fff548ac775
Thomas Cassou <thomas.cassou@booking.com>
2017-10-22 17:36:18 +0200

    add pypi setup files

---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+
+setup(
+  name = 'mapsplotlib',
+  packages = ['mapsplotlib'],
+  version = '1.0.0',
+  description = 'Custom Python plots on a Google Maps background. A flexible matplotlib like interface to generate many types of plots on top of Google Maps.',
+  url = 'https://github.com/tcassou/mapsplotlib',
+  download_url = 'https://github.com/tcassou/mapsplotlib/archive/1.0.0.tar.gz',
+  keywords = ['google', 'maps', 'matplotlib', 'python', 'plot'],
+  license='MIT',
+  classifiers = [
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering :: Visualization',
+  ],
+)
